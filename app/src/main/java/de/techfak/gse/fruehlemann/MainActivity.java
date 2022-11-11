9a3a19fe2e91ed44d3c3008db08a2c9ef4829c8f
Fynn Jonathan Rühlemann <fruehlemann@techfak.uni-bielefeld.de>
2022-11-11 18:03:24 +0100

    refs #47995 @0:20 -- MainActivity.java: ActionListener erstellt, der überprüft, ob etwas im Dropdown Menü ausgewählt wurde wenn das Spiel durch Klick des Buttons g estartet werden soll und Fehlermeldung ausgibt, wenn nichts ausgewählt wurde. -- activity_main.xml: Änderung der id's von Button und Spinner zur besseren Zuordnung.

--- app/src/main/java/de/techfak/gse/fruehlemann/MainActivity.java
+++ app/src/main/java/de/techfak/gse/fruehlemann/MainActivity.java
@@ -3,6 +3,11 @@ package de.techfak.gse.fruehlemann;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onStartClick(View view) {
+        Spinner dropdown = (Spinner) findViewById(R.id.dropdown);
+
+        if(dropdown.getSelectedItem() == null){
+            Snackbar.make(view, "Keine Karte ausgewählt!", Snackbar.LENGTH_SHORT).show();
+        }
     }
 }
