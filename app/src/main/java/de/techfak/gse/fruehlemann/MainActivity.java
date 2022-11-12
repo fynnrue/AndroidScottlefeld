de98d2dd5b5bb194cfc9fc8dacc5b00be343b9e7
Fynn Jonathan Rühlemann <fruehlemann@techfak.uni-bielefeld.de>
2022-11-12 13:58:23 +0100

    refs #47996 @2:00h --MainActivity.java: Auslesen und speichern der Karten aus dem res/raw Ordner, welche anschließend dann mit einem 'neutralen' Wert in das Dropdown Menü als Auswahlmöglichkeiten gesetzt werden. --activity_main.xml: Zentrierung des Textes im Dropdown Menü.

--- app/src/main/java/de/techfak/gse/fruehlemann/MainActivity.java
+++ app/src/main/java/de/techfak/gse/fruehlemann/MainActivity.java
@@ -3,24 +3,41 @@ package de.techfak.gse.fruehlemann;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import java.lang.reflect.Field;
+
 public class MainActivity extends AppCompatActivity {
+    Spinner dropdown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dropdown = findViewById(R.id.dropdown);
+
+        Field[] maps = R.raw.class.getFields(); //alle Maps erhalten
+        String[] mapNames = new String[maps.length+1];
+        mapNames[0] = "Karte auswählen";
+        for(int i = 0; i < maps.length; i++){
+            mapNames[i+1] = maps[i].getName();
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, mapNames);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropdown.setAdapter(adapter);
     }
 
     public void onStartClick(View view) {
-        Spinner dropdown = (Spinner) findViewById(R.id.dropdown);
-
-        if(dropdown.getSelectedItem() == null){
+        if(dropdown.getSelectedItem() == "Karte auswählen"){
             Snackbar.make(view, "Keine Karte ausgewählt!", Snackbar.LENGTH_SHORT).show();
         }
     }
