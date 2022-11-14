743935c53b797110281b036ec7b313785c8d7e84
Fynn Jonathan Rühlemann <fruehlemann@techfak.uni-bielefeld.de>
2022-11-14 13:33:39 +0100

    refs #48062 @0:15h  --GameActivity.java: onBackPressed Methode überschrieben und durch Alert-Dialog eine Abfrage durchgeführt, ob zur Startansicht zurückgekehrt werden soll. Die Activity wird gewechselt und die Spielansicht Activity wird danach beendet.  -- .gitignore: Korrigiert, zuvor Datei (checkstlye-idea.xml) an falscher Stelle gesucht, da ein Folder zu weit geschaut wurde. Wird jetzt richtig ignoriert.

--- app/src/main/java/de/techfak/gse/fruehlemann/GameActivity.java
+++ app/src/main/java/de/techfak/gse/fruehlemann/GameActivity.java
@@ -1,10 +1,10 @@
 package de.techfak.gse.fruehlemann;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.app.AlertDialog;
 import java.lang.reflect.Field;
 
 public class GameActivity extends AppCompatActivity {
@@ -13,5 +13,22 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+
+    @Override
+    public void onBackPressed(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Spiel verlassen").
+                setMessage("Zur Startansicht zurückkehren?").
+                setPositiveButton("Ja", (dialog, id) -> {
+                    Intent mainI = new Intent(GameActivity.this, MainActivity.class);
+                    startActivity(mainI);
+                }).
+                setNegativeButton("Nein", (dialog, id) -> {});
+        AlertDialog alert = builder.create();
+        alert.show();
+
+        finish();
     }
 }
