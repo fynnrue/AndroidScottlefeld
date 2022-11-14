085aa3c88db7777749d593fb346be3f3ca95f003
Fynn Jonathan Rühlemann <fruehlemann@techfak.uni-bielefeld.de>
2022-11-14 18:26:23 +0100

    closes #48001 @0:15h  --GameActivity.java: In Startansicht ausgewählte Datei wird mit BufferedReader und InputStreamReader eingelesen und anschließend durch einen ObjectMapper in einen Tree aus JsonNode's umgewandelt.  --build.gradle: Um Dependency erweitert, um ObjectMapper benutzen zu können. (Siehe GeoJSON Übung)

--- app/src/main/java/de/techfak/gse/fruehlemann/GameActivity.java
+++ app/src/main/java/de/techfak/gse/fruehlemann/GameActivity.java
@@ -2,10 +2,19 @@ package de.techfak.gse.fruehlemann;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AlertDialog;
-import java.lang.reflect.Field;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.util.stream.Collectors;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -13,10 +22,23 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        String map = getIntent().getStringExtra("map");
+
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(getResources().openRawResource(getResources().getIdentifier(map, "raw", getPackageName()))));
+        String jsonContent = br.lines().collect(Collectors.joining());
+
+        ObjectMapper om = new ObjectMapper();
+        try {
+            JsonNode root = om.readTree((jsonContent));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void onBackPressed(){
+    public void onBackPressed() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
         builder.setTitle("Spiel verlassen").
@@ -25,7 +47,8 @@ public class GameActivity extends AppCompatActivity {
                     Intent mainI = new Intent(GameActivity.this, MainActivity.class);
                     startActivity(mainI);
                 }).
-                setNegativeButton("Nein", (dialog, id) -> {});
+                setNegativeButton("Nein", (dialog, id) -> {
+                });
         AlertDialog alert = builder.create();
         alert.show();
 
