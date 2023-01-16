a28d493acc4b1ab97c0bd17081bfccccfd615d6b
Fynn Jonathan Rühlemann <fruehlemann@techfak.uni-bielefeld.de>
2023-01-16 20:16:55 +0100

    refs #55389 @0:30h --build.gradle/AndroidManifest.xml: Nutzung von Serverelementen ermoeglicht durch Einfuegen von Dependencies und notwendigen Android permissions. --acitivity_main.xml: Einfuegen von neuen Buttons und Textfeldern und zwischen Einzel- und Mehrspielerspiel zu waehlen und benoetigte zugehoerige Infos anzuzeigen. --MainActivity.java: Interaktion mit neuen Buttons hinzugefuegt.

--- app/src/main/java/de/techfak/gse/fruehlemann/activities/MainActivity.java
+++ app/src/main/java/de/techfak/gse/fruehlemann/activities/MainActivity.java
@@ -9,12 +9,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.snackbar.Snackbar;
 
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.reflect.Field;
+import java.util.Objects;
 
 import de.techfak.gse.fruehlemann.R;
 import de.techfak.gse.fruehlemann.exceptions.NoMapSelectedException;
@@ -28,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dropdown = findViewById(R.id.dropdown);
+        dropdown = findViewById(R.id.mapSpinner);
         noSelection = "Karte auswählen";
 
         Field[] maps = R.raw.class.getFields();
@@ -59,6 +69,74 @@ public class MainActivity extends AppCompatActivity {
             Snackbar.make(view, "Keine Karte ausgewählt!", Snackbar.LENGTH_SHORT).show();
         }
     }
+
+    public void onSingleplayerClick(View view) {
+        Button singleButton = findViewById(R.id.singleplayerGameButton);
+        Button multiButton = findViewById(R.id.multiplayerGameButton);
+        Button startButton = findViewById(R.id.start);
+        Spinner mapSpinner = findViewById(R.id.mapSpinner);
+
+        singleButton.setVisibility(View.INVISIBLE);
+        multiButton.setVisibility(View.INVISIBLE);
+        startButton.setVisibility(View.VISIBLE);
+        mapSpinner.setVisibility(View.VISIBLE);
+    }
+
+    public void onMultiplayerClick(View view) {
+        Button singleButton = findViewById(R.id.singleplayerGameButton);
+        Button multiButton = findViewById(R.id.multiplayerGameButton);
+        Button connectButton = findViewById(R.id.connectServerButton);
+        TextView urlHeader = findViewById(R.id.textUrlHeader);
+        TextView hostnameHeader = findViewById(R.id.textHostnameHeader);
+        EditText hostnameContent = findViewById(R.id.textHostname);
+        TextView portHeader = findViewById(R.id.textPortHeader);
+        EditText portContent = findViewById(R.id.textPort);
+        TextView welcomeMessage = findViewById(R.id.header);
+
+
+        singleButton.setVisibility(View.INVISIBLE);
+        multiButton.setVisibility(View.INVISIBLE);
+        welcomeMessage.setVisibility(View.INVISIBLE);
+        urlHeader.setVisibility(View.VISIBLE);
+        hostnameHeader.setVisibility(View.VISIBLE);
+        hostnameContent.setVisibility(View.VISIBLE);
+        portHeader.setVisibility(View.VISIBLE);
+        portContent.setVisibility(View.VISIBLE);
+        connectButton.setVisibility(View.VISIBLE);
+
+    }
+
+    public void onConnectClick(View view) {
+        EditText hostnameInput = findViewById(R.id.textHostname);
+        EditText portInput = findViewById(R.id.textPort);
+
+        String hostname = Objects.requireNonNull(hostnameInput.getText()).toString();
+        String port = Objects.requireNonNull(portInput.getText()).toString();
+
+        String url = hostname + ":" + port;
+        StringRequest request = buildRequest(url);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
+    }
+
+    public StringRequest buildRequest(String url) {
+        url += "/";
+        // url somit "http://127.0.0.1:8080/"
+        Response.Listener<String> onResponse = response -> {
+            Toast.makeText(this, response, Toast.LENGTH_SHORT).show();
+        };
+        final Response.ErrorListener onError = error -> {
+            Toast.makeText(this, new String(error.networkResponse.data), Toast.LENGTH_SHORT).show();
+        };
+
+        StringRequest request = new StringRequest(Request.Method.GET, url, onResponse, onError);
+        // request.getBody() liefert = null
+
+        return request;
+    }
+
+
 
     private boolean isMapSelected() {
         try {
